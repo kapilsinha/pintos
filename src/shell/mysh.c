6e76b41cdeb1a4d4c8c6d2b9767e8bfb06d1b6aa
Joseph Tilahun <jtilahun@caltech.edu>
2018-10-08 08:29:42 -0700

    Added ability to convert a sequence of command tokens into a sequence of one or more command structs

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -11,20 +11,25 @@
 // maximum filename length for Linux is 256 chars
 #define MAX_FILENAME_SIZE 256
 
-struct Command {
-    char input[MAX_FILENAME_SIZE];
-    char output[MAX_FILENAME_SIZE];
-    char error[MAX_FILENAME_SIZE];
-    int num_tokens;
+typedef struct command {
+    int input;
+    int output;
+    int error;
+    int num_args;
     char **args;
-};
+} Command;
 
-struct TokenPair {
+typedef struct commands {
+    int num_commands;
+    Command *commands;
+} Commands;
+
+typedef struct token_pair {
     int num_tokens;
     char **tokens;
-};
+} TokenPair;
 
-struct TokenPair tokenize(char * command) {
+TokenPair tokenize(char * command) {
     int start_index = 0;
     int end_index = 0;
     int in_quotes = 0; // 1 if current character is within quotes, else 0
@@ -87,10 +92,113 @@ struct TokenPair tokenize(char * command) {
             end_index += 1;
         }
     }
-    struct TokenPair pair;
+    TokenPair pair;
     pair.num_tokens = num_tokens;
     pair.tokens = tokens;
     return pair;
+}
+
+Commands generate_commands(TokenPair pair) {
+    Commands commands;
+    commands.num_commands = 1;
+    for (int i = 0; i < pair.num_tokens; i++) {
+        if (strcmp(pair.tokens[i], "|") == 0) {
+            commands.num_commands += 1;
+        }
+    }
+    commands.commands = (Command *)
+                        malloc(commands.num_commands * sizeof(Command));
+
+    for (int i = 0; i < commands.num_commands; i++) {
+        commands.commands[i].input = STDIN_FILENO;
+        commands.commands[i].output = STDOUT_FILENO;
+        commands.commands[i].error = STDERR_FILENO;
+        commands.commands[i].num_args = 0;
+        commands.commands[i].args = (char **) NULL;
+    }
+    int j = 0;
+    int io_redirection = 0;
+    for (int i = 0; i < pair.num_tokens; i++) {
+        if (strcmp(pair.tokens[i], "|") == 0) {
+            j += 1;
+            io_redirection = 0;
+        }
+        else if (strcmp(pair.tokens[i], "<") == 0
+                 || strcmp(pair.tokens[i], ">") == 0) {
+            io_redirection = 1;
+        }
+        else if (!io_redirection) {
+            commands.commands[j].num_args += 1;
+        }
+    }
+    for (int i = 0; i < commands.num_commands; i++) {
+        commands.commands[i].args = (char **)
+                                    malloc((commands.commands[i].num_args + 1)
+                                           * sizeof(char *));
+    }
+    int k = 0;
+    int l = 0;
+    // 1 if immediately after an input redirection symbol, else 0
+    int input_redirection = 0;
+    // 1 if immediately after an output redirection symbol, else 0
+    int output_redirection = 0;
+    for (int i = 0; i < pair.num_tokens; i++) {
+        if (strcmp(pair.tokens[i], "|") == 0) {
+            commands.commands[k].args[l] = (char *) NULL;
+            k += 1;
+            l = 0;
+            input_redirection = 0;
+            output_redirection = 0;
+        }
+        else if (strcmp(pair.tokens[i], "<") == 0) {
+            input_redirection = 1;
+            output_redirection = 0;
+        }
+        else if (strcmp(pair.tokens[i], ">") == 0) {
+            input_redirection = 0;
+            output_redirection = 1;
+        }
+        else if (input_redirection || output_redirection) {
+            char *filename;
+            char *src;
+            size_t len;
+            if (strlen(pair.tokens[i]) >= 2 && pair.tokens[i][0] == '\"'
+                && pair.tokens[i][strlen(pair.tokens[i]) - 1] == '\"') {
+                src = pair.tokens[i] + 1;
+                len = strlen(pair.tokens[i]) - 2;
+            }
+            else {
+                src = pair.tokens[i];
+                len = strlen(pair.tokens[i]);
+            }
+            filename = (char *) malloc((len + 1) * sizeof(char));
+            strncpy(filename, src, len);
+            filename[len] = '\0';
+
+            // TODO: Finish setting up the redirection with the file
+        }
+        else {
+            char *src;
+            size_t len;
+            if (strlen(pair.tokens[i]) >= 2 && pair.tokens[i][0] == '\"'
+                && pair.tokens[i][strlen(pair.tokens[i]) - 1] == '\"') {
+                src = pair.tokens[i] + 1;
+                len = strlen(pair.tokens[i]) - 2;
+            }
+            else {
+                src = pair.tokens[i];
+                len = strlen(pair.tokens[i]);
+            }
+            commands.commands[k].args[l] = (char *)
+                                            malloc((len + 1) * sizeof(char));
+            strncpy(commands.commands[k].args[l], src, len);
+            commands.commands[k].args[l][len] = '\0';
+            l += 1;
+        }
+    }
+    commands.commands[k].args[l] = (char *) NULL;
+
+    return commands;
 }
 
 int main() {
@@ -102,11 +210,25 @@ int main() {
     char command [MAX_COMMAND_LENGTH];
     fgets(command, MAX_COMMAND_LENGTH, stdin);
     
-    struct TokenPair pair = tokenize(command);
-    int num_tokens = pair.num_tokens;
-    char **tokens = pair.tokens;
-    printf("Number of tokens: %d\n", num_tokens);
-    for (int i = 0; i < num_tokens; i++) {
-        printf("%s\n", tokens[i]);
+    TokenPair pair = tokenize(command);
+    Commands commands = generate_commands(pair);
+    int num_commands = commands.num_commands;
+    Command *commands_arr = commands.commands;
+    printf("Number of commands: %d\n", num_commands);
+    printf("========================================"
+           "========================================\n");
+    for (int i = 0; i < num_commands; i++) {
+        printf("Input: %d\n\n", commands_arr[i].input);
+        printf("Output: %d\n\n", commands_arr[i].output);
+        printf("Error: %d\n\n", commands_arr[i].error);
+        printf("Number of arguments: %d\n\n", commands_arr[i].num_args);
+        printf("Arguments:\n");
+        for (int j = 0; j < commands_arr[i].num_args; j++) {
+            printf("%s\n", commands_arr[i].args[j]);
+        }
+        if (i < num_commands - 1) {
+            printf("----------------------------------------"
+                   "----------------------------------------\n");
+        }
     }
 }
