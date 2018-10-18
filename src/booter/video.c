4af14dcfd6a04eb4cb5b236f74f2de695826d221
Joseph Tilahun <jtilahun@caltech.edu>
2018-10-17 18:30:37 -0700

    Added video capabilities

--- src/booter/video.c
+++ src/booter/video.c
@@ -1,4 +1,5 @@
 #include "video.h"
+#include <stdint.h>
 
 /* This is the address of the VGA text-mode video buffer.  Note that this
  * buffer actually holds 8 pages of text, but only the first page (page 0)
@@ -20,16 +21,65 @@
  */
 #define VIDEO_BUFFER ((void *) 0xB8000)
 
-
-/* TODO:  You can create static variables here to hold video display state,
- *        such as the current foreground and background color, a cursor
- *        position, or any other details you might want to keep track of!
- */
-
+static char foreground_color;
+static char background_color;
+static uint16_t *video_buffer;
+static Position cursor_pos;
 
 void init_video(void) {
-    /* TODO:  Do any video display initialization you might want to do, such
-     *        as clearing the screen, initializing static variable state, etc.
-     */
+    foreground_color = LIGHT_GRAY;
+    background_color = BLACK;
+    video_buffer = VIDEO_BUFFER;
+    cursor_pos.x = INIT_POS_CURSOR_X;
+    cursor_pos.y = INIT_POS_CURSOR_Y;
 }
 
+void print_string(const char *str) {
+    while (*str != '\0') {
+        uint16_t info;
+        
+        if (cursor_pos.x < WIDTH - 1) {
+            info = (background_color << 12) | (foreground_color << 8) | *str;
+            *video_buffer = info;
+            video_buffer++;
+            str++;
+            cursor_pos.x++;
+        }
+        else if (cursor_pos.y < HEIGHT - 1) {
+            cursor_pos.x = 0;
+            cursor_pos.y++;
+        }
+        else {
+            clear_screen();
+        }
+    }
+}
+
+void clear_screen(void) {
+    uint16_t *video_buffer_eraser = VIDEO_BUFFER;
+    
+    for (int i = 0; i < WIDTH * HEIGHT; i++) {
+        *video_buffer_eraser = 0;
+        video_buffer_eraser++;
+    }
+    
+    video_buffer = VIDEO_BUFFER;
+    cursor_pos.x = INIT_POS_CURSOR_X;
+    cursor_pos.y = INIT_POS_CURSOR_Y;
+}
+
+char get_foreground_color(void) {
+    return foreground_color;
+}
+
+void set_foreground_color(char color) {
+    foreground_color = color;
+}
+
+char get_background_color(void) {
+    return background_color;
+}
+
+void set_background_color(char color) {
+    background_color = color;
+}
--- src/booter/video.h
+++ src/booter/video.h
@@ -22,8 +22,25 @@
 #define YELLOW        14
 #define WHITE         15
 
+#define WIDTH  80
+#define HEIGHT 25
+
+#define INIT_POS_CURSOR_X 0
+#define INIT_POS_CURSOR_Y 0
+
+
+typedef struct position {
+    char x;
+    char y;
+} Position;
+
 
 void init_video(void);
-
+void print_string(const char *str);
+void clear_screen(void);
+char get_foreground_color(void);
+void set_foreground_color(char color);
+char get_background_color(void);
+void set_background_color(char color);
 
 #endif /* VIDEO_H */
