499dad0b40edf93aaa9fd002ede8422f5744ce57
Joseph Tilahun <jtilahun@caltech.edu>
2018-10-18 18:00:06 -0700

    Added basics of game logic

--- src/booter/game.c
+++ src/booter/game.c
@@ -1,3 +1,12 @@
+#include <unistd.h>
+
+#include "game.h"
+#include "video.h"
+#include "keyboard.h"
+#include "interrupts.h"
+
+static Game game;
+
 /* This is the entry-point for the game! */
 #include "interrupts.h"
 #include "keyboard.h"
@@ -8,18 +17,137 @@ void c_start(void) {
      *        enable_interrupts() to start interrupt handling, and go on to
      *        do whatever else you decide to do!
      */
-     init_interrupts();
-     init_keyboard();
-     enable_interrupts();
-
+    init_video();
+    init_keyboard();
+    
+    enable_interrupts();
+    clear_screen();
+    print_string(welcome_screen);
+    
+    while (!is_a_pressed()) {
+        
+    }
+    
+    clear_screen();
+    sleep(NUM_SECONDS_INIT_PAUSE);
+    init_game();
+    
     /* Loop forever, so that we don't fall back into the bootloader code. */
-    int ye = 69;
     while (1) {
-        if (is_z_pressed()) {
-            ye++;
-            if (ye == 101) {
-                ye -= 1;
+        while (!is_done(game)) {
+            run();
+        }
+    }
+}
+
+void init_game(void) {
+    game.player_1.score = 0;
+    game.player_1.paddle.length = PADDLE_LENGTH;
+    game.player_1.paddle.position.x = INIT_POS_P1_X;
+    game.player_1.paddle.position.y = INIT_POS_P1_Y;
+    game.player_1.paddle.velocity.x = INIT_VEL_P1_X;
+    game.player_1.paddle.velocity.y = INIT_VEL_P1_Y;
+    
+    game.player_2.score = 0;
+    game.player_2.paddle.length = PADDLE_LENGTH;
+    game.player_2.paddle.position.x = INIT_POS_P2_X;
+    game.player_2.paddle.position.y = INIT_POS_P2_Y;
+    game.player_2.paddle.velocity.x = INIT_VEL_P2_X;
+    game.player_2.paddle.velocity.y = INIT_VEL_P2_Y;
+    
+    game.ball.position.x = WIDTH / 2;
+    game.ball.position.y = HEIGHT / 2;
+    game.ball.velocity.x = 1;
+    game.ball.velocity.y = 1;
+}
+
+void run(void);
+    compute_game_frame();
+    render_game_frame();
+}
+
+
+void compute_game_frame(void) {
+    if (is_winner(game.player_1)) {
+        clear_screen();
+        print_string("Player 1 wins!");
+    }
+    if (is_winner(game.player_2)) {
+        clear_screen();
+        print_string("Player 2 wins!");
+    }
+ 
+
+    if (is_collision(game.player_1.paddle, game.ball)) {
+        game.ball.velocity.x = -game.ball.velocity.x;
+        game.ball.velocity.y += game.player_1.paddle.velocity.y;
+    }
+    if (is_collision(game.player_2.paddle, game.ball)) {
+        game.ball.velocity.x = -game.ball.velocity.x;
+        game.ball.velocity.y += game.player_2.paddle.velocity.y;
+    }
+    
+    if (is_a_pressed()) {
+        game.player_1.paddle.velocity.x--;
+    }
+    if (is_z_pressed()) {
+        game.player_1.paddle.velocity.x++;
+    }
+    if (is_k_pressed()) {
+        game.player_2.paddle.velocity.x--;
+    }
+    if (is_m_pressed()) {
+        game.player_2.paddle.velocity.x++;
+    }
+    
+    if (has_scored_player_1(game.ball)) {
+        game.player_1.score++;
+    }
+    if (has_scored_player_2(game.ball)) {
+        game.player_2.score++;
+    }
+    
+    game.player_1.paddle.position.x += game.player_1.paddle.velocity.x;
+    game.player_1.paddle.position.y += game.player_1.paddle.velocity.y;
+    game.player_2.paddle.position.x += game.player_2.paddle.velocity.x;
+    game.player_2.paddle.position.y += game.player_2.paddle.velocity.y;
+    
+    game.ball.position.x += game.ball.velocity.x;
+    game.ball.position.y += game.ball.velocity.y;
+}
+
+void render_game_frame(void) {
+    char frame[HEIGHT * WIDTH];
+    
+    memset(&frame, 0, HEIGHT * WIDTH * sizeof(char));
+
+    for (int p = 0; p < game.player_1.paddle.length; p++) {
+        frame[p * WIDTH + game.player_1.paddle.position.x] = '|';
+    }
+    
+    for (int p = 0; p < game.player_2.paddle.length; p++) {
+        frame[p * WIDTH + game.player_2.paddle.position.x] = '|';
+    }
+    
+    frame[game.ball.position.y * WIDTH + game.ball.position.x] = 'X';
+    
+    write_string(frame);
+}
+
+int is_collision(Paddle paddle, Ball ball) {
+    for (int x = paddle.position.x - 1;
+         x <= paddle.position.x + paddle.length + 1; x++) {
+        for (int y = paddle.position.y - 1; y <= paddle.position.y + 1; y++) {
+            if ((x == ball.position.x && (x >= 0 && x < WIDTH))
+                && (y == ball.position.y && (y >= 0 && y < HEIGHT))) {
+                return 1;
             }
         }
     }
+    
+    return 0;
+}
+
+int is_winner(Player player) {
+    return player.score = MAX_SCORE;
 }
---
+++ src/booter/game.h
@@ -0,0 +1,53 @@
+#include "video.h"
+
+#define NUM_SECONDS_INIT_PAUSE 1
+#define INTERVAL 18
+
+#define MAX_SCORE 11
+
+#define INIT_POS_P1_X 0
+#define INIT_POS_P1_Y 0
+#define INIT_VEL_P1_X 0
+#define INIT_VEL_P1_Y 0
+
+#define INIT_POS_P2_X 0
+#define INIT_POS_P2_Y 79
+#define INIT_VEL_P2_X 0
+#define INIT_VEL_P2_Y 0
+
+#define PADDLE_LENGTH 3
+
+#define TIMER_INTERRUPT 0x20
+#define KEYBOARD_INTERRUPT 0x21
+
+const char *welcome_screen = "PONG: Press A to start";
+
+typedef struct velocity {
+    int x;
+    int y;
+} Velocity;
+
+typedef struct paddle {
+    int length;
+    Position position;
+    Velocity velocity;
+} Paddle;
+
+typedef struct player {
+    int score;
+    Paddle paddle;
+} Player;
+
+typedef struct ball {
+    Position position;
+    Velocity velocity;
+} Ball;
+
+typedef struct game {
+    Player player_1;
+    Player player_2;
+    Ball ball;
+} Game;
+
+void init_game(void);
+void render_frame(void);
