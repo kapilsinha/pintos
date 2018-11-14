37630be0153150349bcddf0f5986112e52e392ba
Kapil Sinha <ksinha@caltech.edu>
2018-11-13 20:13:35 -0800

    Write function to print out process termination messages
    Function exit_with_status should be called when a thread is going
    to exit.

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,10 +1,15 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
-void syscall_init(void);
+int valid_pointer(void *vaddr);
+uint32_t pop_stack(struct intr_frame *f);
+void exit_with_status(int status);
 
+void syscall_init(void);
 void sys_halt(void);
+void sys_exit(int status);
 int sys_write(int fd, const void *buffer, unsigned size);
 void sys_nosys(void);
+void syscall_handler(struct intr_frame *);
 
 #endif /* userprog/syscall.h */
