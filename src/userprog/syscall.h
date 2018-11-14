af5a6814f5220463abf0a60143df546ca309028a
Kapil Sinha <ksinha@caltech.edu>
2018-11-13 20:50:17 -0800

    Rearranged method declarations to allow the program to compile

--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,7 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 int valid_pointer(void *vaddr);
-uint32_t pop_stack(struct intr_frame *f);
 void exit_with_status(int status);
 
 void syscall_init(void);
@@ -10,6 +9,5 @@ void sys_halt(void);
 void sys_exit(int status);
 int sys_write(int fd, const void *buffer, unsigned size);
 void sys_nosys(void);
-void syscall_handler(struct intr_frame *);
 
 #endif /* userprog/syscall.h */
