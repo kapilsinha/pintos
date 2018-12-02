b29b6af0ea1499d55f6df2307a1532c65882e747
Karthik Nair <karthiknair97@live.com>
2018-12-01 17:47:37 -0800

    Implement page reclamation on process exit
    In addition to destroying the current thread's page directory,
    process_exit now also marks all of the frames being used by the process
    as unused.
    
    get_frame_entry() relies on the definition of the frame_table_entry
    struct, thus any modifications to the frame_table_entry struct may
    affect the accuracy of this function.

--- src/vm/frame.h
+++ src/vm/frame.h
@@ -3,14 +3,16 @@
 
 /*! Entry for the frame table. */
 struct frame_table_entry {
-    int in_use;                 // Whether this frame is being used or not
-    struct lock *pin;           // Whether this page is pinned or not
     void *frame;                // Pointer to the frame for this entry
     void *page;                 // Pointer to the page for this entry, if used
+    int in_use;                 // Whether this frame is being used or not
+    struct lock pin;            // Whether this page is pinned or not
     struct thread *t;           // Thread that is currently using this frame
 };
 
 struct frame_table_entry *frame_table;
+
+struct frame_table_entry *get_frame_entry(void *frame);
 
 void frame_table_init(size_t user_pages);
 void *frame_get_page(void);
