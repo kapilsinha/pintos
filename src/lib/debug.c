0ec1e5ae837ec3dc7b81635f44231a3fb073098c
Karthik Nair <karthiknair97@live.com>
2018-12-03 15:27:41 -0800

    Fix compilation issues

--- src/lib/debug.c
+++ src/lib/debug.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <string.h>
 
+
 /*! Prints the call stack, that is, a list of addresses, one in
     each of the functions we are nested within.  gdb or addr2line
     may be applied to kernel.o to translate these into file names,
@@ -41,15 +42,5 @@ void debug_word_dump(void *addr, int length) {
     while (curr < end) {
         printf("Address: %#04x -- Value: %#04x\n", (unsigned int) curr, *curr);
         curr++;
-    }
-}
-
-/* Prints out everything in the hash table. */
-void print_hash_table(struct hash *h, int bucket_idx) {
-    struct list *bucket = &h->buckets[bucket_idx];
-    for (struct list_elem *i = list_begin(bucket); i != list_end(bucket); i = list_next(i)) {
-        struct hash_elem *hi = list_elem_to_hash_elem(i);
-        struct supp_page_table_entry *e = hash_entry (hi, struct supp_page_table_entry, elem);
-        printf("%p\n", e->page_addr);
     }
 }
